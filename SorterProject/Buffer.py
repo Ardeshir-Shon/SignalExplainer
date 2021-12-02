ac50f9e649e770ce17a2af998c34afe631f3d614
Ardeshir <ad.ardeshir@gmail.com>
2021-12-02 14:43:17 -0800

    Add core code of sorting project - simulation

---
+++ SorterProject/Buffer.py
@@ -0,0 +1,5 @@
+from Product import Product
+
+class Buffer():
+    def __init__(self,length:int,width:int):
+        
---
+++ SorterProject/Engine.py
@@ -0,0 +1,7 @@
+from Product import Product
+from Belt import Belt
+from Pallet import Pallet
+
+
+product = Product(34,7)
+print(product.getArrivalTime())
