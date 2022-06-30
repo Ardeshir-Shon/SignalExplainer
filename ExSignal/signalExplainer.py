04ebaa282ddaac676e9f0dd0231ec41a2346a21b
Ardeshir <ad.ardeshir@gmail.com>
2022-06-29 19:29:25 -0700

    First stable version.

--- ExSignal/signalExplainer.py
+++ ExSignal/signalExplainer.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import random
 import tensorflow as tf
 
@@ -31,6 +32,7 @@ import time
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.collections import PolyCollection
 from matplotlib import colors as mcolors
+import matplotlib.transforms as mtrans
 import urllib.request
 
 from keras.models import load_model
@@ -46,8 +48,8 @@ from numpy import load
 # model_file = 'model_convlstm.h5'
 # data_file = 'xFake_30_n.npz'
 
-model_file = 'multi_convlstm.h5'
-data_file = 'dataset.npz'
+model_file = 'model_convlstm.h5'
+data_file = 'xFake_1730.npz'
 
 model=load_model(model_file)
 model.summary()
@@ -57,7 +59,13 @@ model.summary()
 dict_data = load(data_file)
 # extract the first array
 print(type(dict_data))
-xFake = dict_data['name1']
+xFake = dict_data['X']
+labels = dict_data['y']
+channel_names = dict_data['channel_names']
+class_names = dict_data['class_names']
+print()
+
+# xFake = dict_data['arr_0']
 
 X_M1_One = xFake
 
@@ -68,54 +76,33 @@ classes_M1_One = np.argmax(yPred_M1_One, axis=1)
 
 numberOfSamples,signalLength,numberOfChannels = xFake.shape
 
-numberOfExplainerFeeds = min(int(numberOfSamples*0.25),125)
+numberOfExplainerFeeds = min(int(numberOfSamples*0.25),450)
 
 explainer = shap.GradientExplainer(model, xFake[:numberOfExplainerFeeds])
 
-numberOfExplainedSamples = 5
+numberOfExplainedSamples = 50
 
 shvalues = explainer.shap_values(xFake[numberOfExplainerFeeds+1:numberOfExplainerFeeds+numberOfExplainedSamples+1])
+actualLabels = labels[numberOfExplainerFeeds+1:numberOfExplainerFeeds+numberOfExplainedSamples+1]
 
-print(len(shvalues))
-
-print(shvalues[0].shape)
-
-
-# c0 = shvalues[0]
-# c1 = shvalues[1]
-# c2 = shvalues[2]
-# c3 = shvalues[3]
-# c4 = shvalues[4]
-# c5 = shvalues[5]
-# # print(type(c0))
-# # print(c0[0,:,0])
-
-# print(min(c0[0,:,0]))
-
-# print(len(c0[0,:,0]))
-
-
-# plt.plot(c0[0,:,0])
-# plt.show()
 
 numberOfClasses = len(shvalues)
 
 sample = random.randint(0,numberOfExplainedSamples-1)
 fig, axs = plt.subplots(numberOfClasses,numberOfChannels, dpi=250)
-fig.suptitle("Shap values for a sample which it's label is "+str(xFake[sample][0]))
 
-fig.text(0.5, 0.04,'#Sample', ha='center')
-fig.text(0.04, 0.5,'Signal Value', va='center', rotation='vertical')
+fig.suptitle("Explaining how the model is identifying class "+r"$\bf{" + str(class_names[int(actualLabels[sample][0])]) + "}$"+"?", fontsize=10)
+
+fig.text(0.5, 0.04,'Channels', ha='center', fontsize=10)
+fig.text(0.04, 0.5,'Classes', va='center', rotation='vertical', fontsize=10)
 
 
 for feature in range(numberOfChannels):
     aggList = []
     for class_ in range(numberOfClasses):
         aggList = list(aggList) + list(shvalues[class_][sample,:,feature])
-    overallMax = abs(max(aggList))
-    # overallMax = abs(max(list(c0[sample,:,feature])+list(c1[sample,:,feature])+list(c2[sample,:,feature])+list(c3[sample,:,feature])+list(c4[sample,:,feature])+list(c5[sample,:,feature]),key=abs))
-
-    # print(y_test[0])
+    overallMax = abs(max(aggList,key=abs))
+    
     for i in range(numberOfClasses):
         reds = []
         blues = []
@@ -123,8 +110,6 @@ for feature in range(numberOfChannels):
         for value in shvalues[i][sample,:,feature]:
             if value >= 0:
                 reds.append(abs(value)/overallMax)
-                if abs(value)/overallMax > 1:
-                    print(value)
                 blues.append(0)
             else:
                 reds.append(0)
@@ -133,15 +118,19 @@ for feature in range(numberOfChannels):
         y = np.arange(signalLength)
         
         if numberOfChannels == 1:
-            axs[i].plot(y,xFake[sample,:,feature],color = "gray")    
+            axs[i].plot(y,xFake[sample,:,feature],color = "gray", lw=0.5, label = "Signal", alpha = 0.5)  
+            axs[0].set_title(str(channel_names[feature]), fontsize=8)
+            axs[i].set_ylabel(class_names[i], fontsize=6)  
         else:
-            axs[i,feature].plot(y,xFake[sample,:,feature],color = "gray")
+            axs[i,feature].plot(y,xFake[sample,:,feature],color = "gray", lw=0.5, label = "Signal", alpha = 0.5)
+            axs[0,feature].set_title(str(channel_names[feature]), fontsize=8)
+            axs[i,0].set_ylabel(class_names[i], fontsize=6)
 
-        # x = c0[0,:,1] #np.array([ v if v < 0 else 0 for v in c0[0,:,1]])
         x = xFake[sample,:,feature]
         y = np.arange(signalLength)
 
         alphas = np.array(blues)
+        
         rgba_colors = np.zeros((signalLength,4))
         # for red the first column needs to be one
         rgba_colors[:,2] = 1
@@ -149,9 +138,9 @@ for feature in range(numberOfChannels):
         rgba_colors[:, 3] = alphas
 
         if numberOfChannels == 1:
-            axs[i].scatter(y, x, s = 15 ,c=rgba_colors)
+            axs[i].scatter(y, x, s = 15/numberOfChannels ,c=rgba_colors)
         else:
-            axs[i,feature].scatter(y, x, s = 15 ,c=rgba_colors.astype(int))
+            axs[i,feature].scatter(y, x, s = 15/numberOfChannels ,c=rgba_colors)
 
 
         # -------------------
@@ -167,7 +156,33 @@ for feature in range(numberOfChannels):
         rgba_colors[:, 3] = alphas
         
         if numberOfChannels == 1:
-            axs[i].scatter(y, x, s = 15 ,c=rgba_colors)
+            axs[i].scatter(y, x, s = 15/numberOfChannels ,c=rgba_colors)
         else:
-            axs[i,feature].scatter(y, x, s = 15 ,c=rgba_colors.astype(int))
+            axs[i,feature].scatter(y, x, s = 15/numberOfChannels ,c=rgba_colors)
+
+# Hide x labels and tick labels for top plots and y ticks for right plots.
+for ax in axs.flat:
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+
+
+#Get the minimum and maximum extent, get the coordinate half-way between those
+if numberOfChannels > 1:
+    r = fig.canvas.get_renderer()
+    get_bbox = lambda ax: ax.get_tightbbox(r).transformed(fig.transFigure.inverted())
+    bboxes = np.array(list(map(get_bbox, axs.flat)), mtrans.Bbox).reshape(axs.shape)
+    ymax = np.array(list(map(lambda b: b.y1, bboxes.flat))).reshape(axs.shape).max(axis=1)
+    ymin = np.array(list(map(lambda b: b.y0, bboxes.flat))).reshape(axs.shape).min(axis=1)
+    ys = np.c_[ymax[1:], ymin[:-1]].mean(axis=1)
+
+    # Draw a horizontal lines at those coordinates
+    for y in ys:
+        line = plt.Line2D([0.1,.9],[y,y], transform=fig.transFigure, color="black")
+        fig.add_artist(line)
+
 plt.show()
